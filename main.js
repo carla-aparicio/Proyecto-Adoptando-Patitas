73cc15d569f7249e3fa5fc3c01a51ba7b3a91877
Carla <154434780+carla-aparicio@users.noreply.github.com>
2023-12-28 12:13:16 -0300

    agregados

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function siguienteFormulario(event) {
+    
+    var elementoActivo = document.getElementsByClassName("activo");
+    var elementoSiguiente = document.querySelector(".activo + .desactivo");
+    //console.log(elementoActivo);
+    elementoActivo[0].classList.add("desactivo");
+    elementoActivo[0].classList.remove("activo");
+    elementoSiguiente.classList.add("activo");
+    elementoSiguiente.classList.remove("desactivo");
+    
+}
+function mostrarResultado() {
+    var nombre = document.getElementById("nombre");
+    alert("hola " + nombre.value + "nos pondremos en contacto con vos para concretar la adopción");
+
+}
